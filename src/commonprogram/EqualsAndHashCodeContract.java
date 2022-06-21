48b5eba9e99e4ea4f78072f9b196030e1bd5771d
saravg2012@gmail.com <saravg2012@gmail.com>
2022-06-20 23:18:27 -0400

    added-interview-question

---
+++ src/commonprogram/EqualsAndHashCodeContract.java
@@ -0,0 +1,44 @@
+package commonprogram;
+
+import pojo.Employee;
+
+import java.sql.SQLOutput;
+import java.util.HashSet;
+import java.util.Set;
+
+public class EqualsAndHashCodeContract {
+    public static void main(String[] args) {
+
+        Employee e1 =  new Employee("sara",55,50000);
+        Employee e2 =  new Employee("sara",55,50000);
+
+        if(e1.equals(e2)) {
+            System.out.println("True");
+        }else {
+            System.out.println("False");
+        }
+
+        Set<Employee> set = new HashSet<>();
+        set.add(e1);
+        set.add(e2);
+
+        System.out.println(set);
+
+        String a ="10";
+        String b ="10";
+
+        if(a == b) {
+            System.out.println("true");
+        }else {
+            System.out.println("False");
+        }
+
+        if(a.equals(b)) {
+            System.out.println("true");
+        }else {
+            System.out.println("False");
+        }
+
+
+    }
+}
